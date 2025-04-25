500ec28ae2c575c68521db5f0757f4d057531123
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-04-25 15:31:26 -0300

    :sparkles: feat: Create Repository for User Model

---
+++ src/main/java/com/delivery_track/api/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.delivery_track.api.repositories;
+
+import com.delivery_track.api.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    @Query(value = "SELECT * FROM users WHERE name = :email")
+    Optional<User> findUserByEmail(@Param("email")String email);
+}
