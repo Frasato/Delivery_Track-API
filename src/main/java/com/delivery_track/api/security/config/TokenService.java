0ff05b4f7e33f3e308b6832a2845d5757bb52203
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-04-25 15:32:39 -0300

    :sparkles: feat: Service to Generate and Validate Tokens

---
+++ src/main/java/com/delivery_track/api/security/config/TokenService.java
@@ -0,0 +1,53 @@
+package com.delivery_track.api.security.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.delivery_track.api.models.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.jwt.secret}")
+    private String secret;
+
+    public String generateToken(User user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("delivery-track")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(expirationDate())
+                    .sign(algorithm);
+            return token;
+        }catch(JWTCreationException jwtException){
+            throw new RuntimeException("ERROR: can't create token for user! "
+                    + jwtException + " Message: " + jwtException.getMessage());
+        }
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("delivery-track")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch(JWTVerificationException jwtVerification){
+            throw new RuntimeException("ERROR: token not valid: " + jwtVerification);
+        }
+    }
+
+    public Instant expirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
