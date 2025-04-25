361f75f6267b418ffd68eba7824095d6c3685444
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-04-25 15:35:54 -0300

    :wrench: chore: Configure Security Filter

---
+++ src/main/java/com/delivery_track/api/security/SecurityFilter.java
@@ -0,0 +1,52 @@
+package com.delivery_track.api.security;
+
+import com.delivery_track.api.models.User;
+import com.delivery_track.api.repositories.UserRepository;
+import com.delivery_track.api.security.config.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException
+    {
+        var token = this.recoverToken(request);
+        var login =tokenService.validateToken(token);
+
+        if(login != null){
+            User user = userRepository.findUserByEmail(login)
+                    .orElseThrow(() -> new RuntimeException("ERROR: can't find user!"));
+            var authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+            var authentication = new UsernamePasswordAuthenticationToken(token, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    public String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer", "");
+    }
+
+}
