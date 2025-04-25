16f104a4834126a45ca94abf476d04df589dfbd4
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-04-25 15:31:35 -0300

    :sparkles: feat: Create Repository for Delivery Model

---
+++ src/main/java/com/delivery_track/api/repositories/DeliveryRepository.java
@@ -0,0 +1,9 @@
+package com.delivery_track.api.repositories;
+
+import com.delivery_track.api.models.Delivery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeliveryRepository extends JpaRepository<Delivery, String> {
+}
