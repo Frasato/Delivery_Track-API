f8e758bb54d6e852a15c56d805be9eeb944ad5de
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-04-26 08:51:01 -0300

    :sparkles: feat: Change websocket to req and res location lat and lng

--- src/main/java/com/delivery_track/api/controllers/WebSocketController.java
+++ src/main/java/com/delivery_track/api/controllers/WebSocketController.java
@@ -1,7 +1,8 @@
 package com.delivery_track.api.controllers;
 
 import com.delivery_track.api.dtos.LocationDto;
-import org.springframework.messaging.Message;
+import com.delivery_track.api.services.DeliveryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -9,9 +10,13 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class WebSocketController {
 
+    @Autowired
+    private DeliveryService deliveryService;
+
     @MessageMapping("/location")
     @SendTo("/topic/location")
     public LocationDto sendMessage(LocationDto locationDto){
+        deliveryService.updateLocation(locationDto.deliveryId(), locationDto.lat(), locationDto.lng());
         return locationDto;
     }
 
