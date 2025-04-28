a3763f44a3657fd79ce53f76aa8106717642de8d
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-04-28 12:43:56 -0300

    :sparkles: feat: Config Cors

--- src/main/java/com/delivery_track/api/controllers/WebSocketController.java
+++ src/main/java/com/delivery_track/api/controllers/WebSocketController.java
@@ -4,11 +4,12 @@ import com.delivery_track.api.dtos.LocationDto;
 import com.delivery_track.api.services.DeliveryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin("*")
 public class WebSocketController {
 
     @Autowired
