944b7274d97936173d014fd5905d23d6de8cd94f
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-04-28 12:46:17 -0300

    :bug: fix: Wrong URI fixed

--- src/main/java/com/delivery_track/api/security/SecurityConfig.java
+++ src/main/java/com/delivery_track/api/security/SecurityConfig.java
@@ -30,8 +30,8 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(
                         SessionCreationPolicy.STATELESS
                 )).authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/delivery/track/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/delivery/track/login").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
