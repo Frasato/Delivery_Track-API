54dd5c085701f84fae0fdb9c28ef4d51de2dfeee
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-04-29 11:59:23 -0300

    :books: docs: Endpoints documentation

--- src/main/java/com/delivery_track/api/controllers/AuthController.java
+++ src/main/java/com/delivery_track/api/controllers/AuthController.java
@@ -6,6 +6,10 @@ import com.delivery_track.api.dtos.ResponseAuthDto;
 import com.delivery_track.api.models.User;
 import com.delivery_track.api.repositories.UserRepository;
 import com.delivery_track.api.security.config.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,6 +20,10 @@ import java.util.Optional;
 @RestController
 @RequestMapping(value = "/api/delivery/track")
 @CrossOrigin("*")
+@Tag(
+        name = "Authenticate",
+        description = "Endpoints to register or login users"
+)
 public class AuthController {
 
     @Autowired
@@ -26,6 +34,14 @@ public class AuthController {
     private PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
+    @Operation(
+            summary = "Register",
+            description = "Create a new user"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Create a new user"),
+            @ApiResponse(responseCode = "400", description = "Invalid Information")
+    })
     public ResponseEntity<?> userRegister(@RequestBody AuthDto authDto){
         if(authDto.email().isEmpty() || authDto.name().isEmpty() || authDto.password().isEmpty()){
             return ResponseEntity.status(400).body("Error: The field name, email or password should be not empty!");
@@ -41,6 +57,14 @@ public class AuthController {
     }
 
     @PostMapping("/login")
+    @Operation(
+            summary = "Login",
+            description = "Find and authenticate user"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Authenticated"),
+            @ApiResponse(responseCode = "400", description = "Invalid Information")
+    })
     public ResponseEntity<?> userLogin(@RequestBody LoginDto loginDto){
         Optional<User> findedUser = userRepository.findUserByEmail(loginDto.email());
 
--- src/main/java/com/delivery_track/api/controllers/DeliveryController.java
+++ src/main/java/com/delivery_track/api/controllers/DeliveryController.java
@@ -5,6 +5,10 @@ import com.delivery_track.api.dtos.LocationDto;
 import com.delivery_track.api.models.Delivery;
 import com.delivery_track.api.repositories.DeliveryRepository;
 import com.delivery_track.api.services.DeliveryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +16,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/delivery/track")
 @CrossOrigin("*")
+@Tag(
+        name = "Delivery",
+        description = "Endpoints to create and finish delivery"
+)
 public class DeliveryController {
 
     @Autowired
@@ -20,18 +28,29 @@ public class DeliveryController {
     private DeliveryService deliveryService;
 
     @PostMapping("/init/{id}")
+    @Operation(
+            summary = "Init",
+            description = "Create a new delivery"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Create a new delivery"),
+            @ApiResponse(responseCode = "400", description = "User not found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public ResponseEntity<?> initDelivery(@PathVariable String id){
         return deliveryService.initDelivery(id);
     }
 
-    @GetMapping("/public/{id}")
-    public ResponseEntity<LocationDto> getDeliveryLocation(@PathVariable String id){
-        Delivery delivery = deliveryRepository.findById(id).orElseThrow(() -> new RuntimeException("ERROR"));
-        LocationDto locationDto = new LocationDto(id, delivery.getLat(), delivery.getLng());
-        return ResponseEntity.ok(locationDto);
-    }
-
     @PostMapping("/finish")
+    @Operation(
+            summary = "Finish",
+            description = "Finish a delivery"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Finished a delivery"),
+            @ApiResponse(responseCode = "400", description = "User not found or Delivery"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public ResponseEntity<?> finishDelivery(@RequestBody FinishDeliveryDto finish){
         return deliveryService.finishDelivery(finish.deliveryId(), finish.userId());
     }
