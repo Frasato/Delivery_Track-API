1e834f7d106225f2e557af79fc4b6202562a53fa
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-04-29 15:09:32 -0300

    :sparkles: feat: Socket channel by delivery id

--- src/main/java/com/delivery_track/api/controllers/WebSocketController.java
+++ src/main/java/com/delivery_track/api/controllers/WebSocketController.java
@@ -3,22 +3,26 @@ package com.delivery_track.api.controllers;
 import com.delivery_track.api.dtos.LocationDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WebSocketController {
 
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
     @MessageMapping("/location")
-    @SendTo("/topic/location")
     @Operation(
             summary = "Websocket",
             description = "Send and get location"
     )
     @SecurityRequirement(name = "bearerAuth")
-    public LocationDto sendMessage(LocationDto locationDto){
-        return new LocationDto(locationDto.deliveryId(), locationDto.lat(), locationDto.lng());
+    public void sendMessage(LocationDto locationDto){
+        String destination = "/topic/location/" + locationDto.deliveryId();
+        simpMessagingTemplate.convertAndSend(destination, locationDto);
     }
 
 }
