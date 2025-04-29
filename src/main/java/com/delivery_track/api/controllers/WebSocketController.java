8ca0179aa6959c2abaa22cb74dc6fa023c4fd324
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-04-29 10:50:31 -0300

    :recycle: refactor: Remove unnecessary lines

--- src/main/java/com/delivery_track/api/controllers/WebSocketController.java
+++ src/main/java/com/delivery_track/api/controllers/WebSocketController.java
@@ -1,26 +1,17 @@
 package com.delivery_track.api.controllers;
 
 import com.delivery_track.api.dtos.LocationDto;
-import com.delivery_track.api.services.DeliveryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
-@CrossOrigin("*")
 public class WebSocketController {
 
-    @Autowired
-    private DeliveryService deliveryService;
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
-
     @MessageMapping("/location")
-    public void sendMessage(LocationDto locationDto){
-        deliveryService.updateLocation(locationDto.deliveryId(), locationDto.lat(), locationDto.lng());
-        simpMessagingTemplate.convertAndSend("/topic/location/" + locationDto.deliveryId(), locationDto);
+    @SendTo("/topic/location")
+    public LocationDto sendMessage(LocationDto locationDto){
+        return new LocationDto(locationDto.deliveryId(), locationDto.lat(), locationDto.lng());
     }
 
 }
