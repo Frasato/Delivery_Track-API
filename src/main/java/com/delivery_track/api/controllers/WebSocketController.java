48382635fe41ebbb740006e92226504398b49f9b
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-05-04 20:09:50 -0300

    :sparkles: feat: Add logger on websocket

--- src/main/java/com/delivery_track/api/controllers/WebSocketController.java
+++ src/main/java/com/delivery_track/api/controllers/WebSocketController.java
@@ -3,6 +3,8 @@ package com.delivery_track.api.controllers;
 import com.delivery_track.api.dtos.LocationDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class WebSocketController {
 
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketController.class);
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
@@ -21,6 +24,7 @@ public class WebSocketController {
     )
     @SecurityRequirement(name = "bearerAuth")
     public void sendMessage(LocationDto locationDto){
+        logger.info("Lat: " + locationDto.lat() + "Lng: " + locationDto.lng());
         String destination = "/topic/location/" + locationDto.deliveryId();
         simpMessagingTemplate.convertAndSend(destination, locationDto);
     }
